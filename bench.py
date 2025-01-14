afd5acb67b2a52f39a6720623bb58bd408f860dd
Riwan <coefficriwan@gmail.com>
2025-01-14 11:23:41 +0100

    feat: bench.py update

--- bench.py
+++ bench.py
@@ -5,30 +5,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-nb_bodies = 1000
-
-benches = [
-    {"variant": "cpu+naive", "bodies": nb_bodies, "iterations": 10},
-    {"variant": "cpu+optim1", "bodies": nb_bodies, "iterations": 10},
-    {"variant": "cpu+optim1_approx", "bodies": nb_bodies, "iterations": 200},
-    {"variant": "simd+naive", "bodies": nb_bodies, "iterations": 1000},
-    {"variant": "simd+optim1", "bodies": nb_bodies, "iterations": 2000},
-    {"variant": "simd+optim2", "bodies": nb_bodies, "iterations": 4000},
-]
-
+nb_bodies = 30000
 passes = 10
 
-data_root = "data"
+data_root = "../data"
 data_output = f"{data_root}/benches.csv"
-graph_output = f"{data_root}/benches.png"
+data_output_omp = f"{data_root}/benches_omp.csv"
 
-time_pattern = r"Entire simulation took ([\d.]+) ms"
-fps_pattern = r"\(([\d.]+) FPS"
-gflops_pattern = r"([\d.]+) Gflop/s"
+patterns = {
+    "time": r"Entire simulation took ([\d.]+) ms",
+    "fps": r"\(([\d.]+) FPS",
+    "gflops": r"([\d.]+) Gflop/s",
+}
+
+benches = [
+    {"variant": "cpu+naive", "bodies": 1000, "iterations": 500},
+    {"variant": "cpu+optim1", "bodies": 1000, "iterations": 500},
+    {"variant": "cpu+optim1_approx", "bodies": 1000, "iterations": 500},
+    {"variant": "cpu+optim1_approx", "bodies": 10000, "iterations": 10},
+    {"variant": "simd+naive", "bodies": 10000, "iterations": 100},
+    {"variant": "simd+optim1", "bodies": 10000, "iterations": 100},
+    {"variant": "simd+optim2", "bodies": 10000, "iterations": 100},
+    {"variant": "simd+omp", "bodies": 10000, "iterations": 200},
+    {"variant": "ocl+naive", "bodies": 30000, "iterations": 200},
+    {"variant": "cuda+naive", "bodies": 30000, "iterations": 200},
+    {"variant": "cuda+optim1", "bodies": 30000, "iterations": 200},
+    {"variant": "cuda+optim1", "bodies": 10000, "iterations": 2000},
+    {"variant": "cuda+optim2", "bodies": 30000, "iterations": 200},
+]
+
+
+def extract_metrics(stdout):
+    return (
+        float(re.search(patterns["time"], stdout).group(1)),
+        float(re.search(patterns["fps"], stdout).group(1)),
+        float(re.search(patterns["gflops"], stdout).group(1)),
+    )
 
 
 def run_simu(variant: str, bodies: int, iterations: int):
-    print("> running variant: ", variant)
+    print("> Running variant: ", variant)
     print(f"{passes} passes of ({iterations} iterations, {bodies} bodies)")
 
     arr_simulation_time = []
@@ -38,7 +54,7 @@ def run_simu(variant: str, bodies: int, iterations: int):
     for _ in range(passes):
         result = subprocess.run(
             [
-                "build/bin/murb",
+                "bin/murb",
                 "-n",
                 str(bodies),
                 "-i",
@@ -52,12 +68,10 @@ def run_simu(variant: str, bodies: int, iterations: int):
             stderr=subprocess.PIPE,
             text=True,
         )
-
-        arr_simulation_time.append(
-            float(re.search(time_pattern, result.stdout).group(1))
-        )
-        arr_fps.append(float(re.search(fps_pattern, result.stdout).group(1)))
-        arr_gflops.append(float(re.search(gflops_pattern, result.stdout).group(1)))
+        time, fps, gflops = extract_metrics(result.stdout)
+        arr_simulation_time.append(time)
+        arr_fps.append(fps)
+        arr_gflops.append(gflops)
 
     simulation_time = np.mean(arr_simulation_time).round(3)
     std_simulation_time = np.std(arr_simulation_time).round(3)
@@ -67,53 +81,174 @@ def run_simu(variant: str, bodies: int, iterations: int):
     std_gflops = np.std(arr_gflops).round(3)
 
     print(
-        f"simulation took {simulation_time} [+- {std_simulation_time}] ms "
+        f"Simulation took {simulation_time} [+- {std_simulation_time}] ms "
         f"({fps} [+- {std_fps}] FPS, {gflops} [+- {std_gflops}] Gflop/s)"
     )
 
-    return (simulation_time, std_simulation_time, fps, std_fps, gflops, std_gflops)
+    return (
+        variant,
+        bodies,
+        simulation_time,
+        std_simulation_time,
+        fps,
+        std_fps,
+        gflops,
+        std_gflops,
+    )
 
 
 def run_benches():
     for bench_config in benches:
         stats = run_simu(**bench_config)
-        gen_data_output(stats)
+        gen_data_output(stats, data_output)
+
+
+def run_omp_simu(bench: str, nb_bodies):
+    print(f"> Running OMP benchmark: {bench}")
+
+    arr_simulation_time = []
+    arr_fps = []
+    arr_gflops = []
+
+    cmd_parts = bench.split()
+    env_var, command = cmd_parts[0], cmd_parts[1:]
+    env_key, env_value = env_var.split("=")
+
+    for _ in range(passes):
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            env={**os.environ, env_key: env_value},
+        )
+
+        time, fps, gflops = extract_metrics(result.stdout)
+        arr_simulation_time.append(time)
+        arr_fps.append(fps)
+        arr_gflops.append(gflops)
+
+    simulation_time = np.nanmean(arr_simulation_time).round(3)
+    std_simulation_time = np.nanstd(arr_simulation_time).round(3)
+    fps = np.nanmean(arr_fps).round(3)
+    std_fps = np.nanstd(arr_fps).round(3)
+    gflops = np.nanmean(arr_gflops).round(3)
+    std_gflops = np.nanstd(arr_gflops).round(3)
+
+    print(
+        f"{env_var[len(env_var) - 1]}: simulation took {simulation_time} [+- {std_simulation_time}] ms "
+        f"({fps} [+- {std_fps}] FPS, {gflops} [+- {std_gflops}] Gflop/s)"
+    )
+    return (
+        env_var[len(env_var) - 1],
+        nb_bodies,
+        simulation_time,
+        std_simulation_time,
+        fps,
+        std_fps,
+        gflops,
+        std_gflops,
+    )
+
+
+def plot_speedup_omp(data, bodies, title):
+    plt.title(title)
+    plt.xlabel("Number of cores")
+    plt.ylabel("Speedup")
+
+    nb_cores = [1, 2, 3, 4, 5, 6]
+    speedup = data / data[0]
+    plt.plot(nb_cores, speedup, "o-", label="Experimental", color="#3586bc")
+    plt.plot(nb_cores, nb_cores, "--", label="Optimal", color="red")
+    plt.legend(["Experimental", "Optimal"])
+    plt.grid(linestyle="dashed")
+    plt.savefig(f"{data_root}/benches_omp{bodies}.png")
+    plt.close()
+
+
+def run_omp_benches():
+    nb_iters = [1000, 500]
+    nb_bodies = [1000, 2000]
+    stats = []
+
+    for i in range(len(nb_bodies)):
+        bodies = nb_bodies[i]
+        iterations = nb_iters[i]
+
+        omp_bench = [
+            f"OMP_NUM_THREADS=1 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+            f"OMP_NUM_THREADS=2 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+            f"OMP_NUM_THREADS=3 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+            f"OMP_NUM_THREADS=4 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+            f"OMP_NUM_THREADS=5 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+            f"OMP_NUM_THREADS=6 ./build/bin/murb -i {iterations} -n {bodies} --nv --gf --im simd+omp",
+        ]
+
+        for bench in omp_bench:
+            bench_stats = run_omp_simu(bench, bodies)
+            stats.append(bench_stats)
+            gen_data_output(bench_stats, data_output_omp)
+
+    data = pd.read_csv(data_output_omp)
+    for bodies in nb_bodies:
+        subset = data[data["bodies"] == bodies]
+        fps_data = subset["fps"].to_numpy()
+        plot_speedup_omp(fps_data[:6], bodies, f"Speedup for {bodies} bodies")
 
 
 def init_data_output():
     os.makedirs(data_root, exist_ok=True)
     with open(data_output, "w") as file:
-        file.write("time,std_time,fps,std_fps,gflops,std_gflops\n")
+        file.write("variant,bodies,time,std_time,fps,std_fps,gflops,std_gflops\n")
+    with open(data_output_omp, "w") as file:
+        file.write("variant,bodies,time,std_time,fps,std_fps,gflops,std_gflops\n")
 
 
-def gen_data_output(data):
+def gen_data_output(data, data_output):
     with open(data_output, "a") as file:
-        file.write(f"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]}\n")
+        file.write(f"{','.join(map(str, data))}\n")
 
 
-def plot_benches():
+def plot_benches(selected_variants=None, output_file_name="benches"):
     data = pd.read_csv(data_output)
-    fps_data = data[["fps", "std_fps"]]
-    fps_data.loc[:, ["variant"]] = [bench["variant"] for bench in benches]
+
+    if selected_variants:
+        data = data[data["variant"].isin(selected_variants)]
+
+    if data.empty:
+        print("Aucune donnée disponible pour les variantes sélectionnées.")
+        return
 
     plt.figure(figsize=(12, 8))
-
     plt.rc("axes", axisbelow=True)
     plt.grid(linestyle="dashed")
 
     plt.bar(
-        fps_data["variant"],
-        fps_data["fps"],
-        yerr=fps_data["std_fps"],
-        capsize=4,
+        data["variant"], data["fps"], yerr=data["std_fps"], capsize=4, color="#4caf50"
     )
     plt.xlabel(f"Simulation optimization variants ({nb_bodies} bodies)")
     plt.ylabel("Frame rate (FPS)")
+    plt.title("Comparaison des performances des variantes")
 
-    plt.savefig(graph_output, dpi=400)
+    output_file = f"{data_root}/{output_file_name}.png"
+    plt.savefig(output_file, dpi=400)
     plt.close()
+    print(f"Graphique sauvegardé sous {output_file}")
 
 
 init_data_output()
 run_benches()
-plot_benches()
+# run_omp_benches()
+plot_benches(["cpu+naive", "cpu+optim1", "cpu+optim1_approx"], "benches_cpu", 1000)
+
+plot_benches(["simd+naive", "simd+optim1", "simd+optim2"], "benches_simd", 10000)
+
+plot_benches(["cuda+naive", "cuda+optim1", "cuda+optim2"], "benches_cuda", 30000)
+
+plot_benches(["ocl+naive", "cuda+naive"], "benches_gpu", 30000)
+
+plot_benches(
+    ["cpu+omptim1_approx", "simd+optim1", "simd+omp", "cuda+omptim1"],
+    "benches_fast",
+    10000,
+)
